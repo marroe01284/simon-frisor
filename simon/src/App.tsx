6bbe2939e098c96be2c50783cff650671f98cf0f
Marius Rønning <mpr941@hotmail.com>
2025-06-13 19:18:21 +0200

    FEAT: Added dummy data

--- simon/src/App.tsx
+++ simon/src/App.tsx
@@ -1,11 +1,19 @@
+import Aapningstider from "./components/Aapningstider";
+import Anmeldelser from "./components/Anmeldelser";
+import Besok from "./components/Besøk";
 import Hero from "./components/Hero";
 import Navbar from "./components/Nav";
+import Priser from "./components/Priser";
 
 function App() {
   return (
     <div>
     <Navbar/>
     <Hero/>
+    <Priser/>
+    <Aapningstider/>
+    <Anmeldelser/>
+    <Besok/>
     </div>
   );
 }
---
+++ simon/src/components/Aapningstider.tsx
@@ -0,0 +1,12 @@
+export default function Aapningstider(){
+    return(
+        <section id="aapningstider" className="bg-black py-16 px-4 text-center">
+        <h2 className="text-3xl font-bold text-white mb-8">Åpningstider</h2>
+        <ul className="space-y-2 text-white">
+          <li>Mandag - Fredag: 09:00 - 17:00</li>
+          <li>Lørdag: 10:00 - 14:00</li>
+          <li>Søndag: Stengt</li>
+        </ul>
+      </section>
+    )
+}

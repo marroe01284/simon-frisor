220dffcf191b4764b8f3c2237456b7a4eb331520
Marius Rønning <mpr941@hotmail.com>
2025-06-13 16:53:58 +0200

    FEAT: added tailwind

--- simon/src/App.tsx
+++ simon/src/App.tsx
@@ -1,8 +1,9 @@
-
-import './App.css'
-
 function App() {
-
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + React + TypeScript!</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
